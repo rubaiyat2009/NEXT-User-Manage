eda0b719aee601cded402ce932c9c6c02ae02e09
rubaiyat2009 <rubaiyat2009@gmail.com>
2023-10-26 02:32:16 +0800

    remove unnecessary items

--- src/app/_models/index.ts
+++ src/app/_models/index.ts
@@ -1,14 +1,6 @@
 import '@/app/_models/plugins';
 // Append model here
-export * from '@/app/_models/ExceptionTimeSlot';
-export * from '@/app/_models/OperationTimeSlot';
 export * from '@/app/_models/User';
-export * from '@/app/_models/PickupRequest';
-export * from '@/app/_models/Booking';
-export * from '@/app/_models/Shipper';
-export * from '@/app/_models/Haulier';
-export * from '@/app/_models/Depot';
 export * from '@/app/_models/Key';
 export * from '@/app/_models/Session';
 export * from '@/app/_models/User';
-export * from '@/app/_models/Invoice';
